ab4086ef95bea0c59e1696832ac116620b7063c2
yasuiyasumasa <hikkosisimasita20160821@gmail.com>
2022-02-28 23:25:32 +0900

    secomd commit

---
+++ resources/views/admin/book/edit.blade.php
@@ -0,0 +1,13 @@
+extends('layouts.admin')
+
+@section('title', '書籍の登録')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>書籍の登録</h2>
+      </div>
+    </div>
+  </div>
+@endsection
