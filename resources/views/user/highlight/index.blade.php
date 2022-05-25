cc8cbf1f092f9f657b58b1c1619d789309036e44
yasuiyasumasa <hikkosisimasita20160821@gmail.com>
2022-05-25 16:14:04 +0900

    2022/05/25

--- resources/views/user/highlight/index.blade.php
+++ resources/views/user/highlight/index.blade.php
@@ -1,118 +1,20 @@
 @extends('layouts.user')
 @section('title', 'ハイライト')
 @section('content')
-<div class="card-columns bg-light p-4">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS000005.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h6 class="card-title">タイトル</h6>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
+<div class="card" style="width: 18rem;">
+  <img src="http://localhost/bricolage/public/image/book_cover/WS000001.JPG" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS000006.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h6 class="card-title">タイトル</h6>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
-  </div>
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS000007.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
-  </div>
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS000008.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
-  </div>
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS000009.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
-  </div>
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS0000010.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
-  </div>
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="http://localhost/bricolage/public/image/book_cover/WS0000011.JPG" alt="" class="card-img">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <h6 class="card-author">著者</h6>
-        <h6 class="card-publisher">出版社</h6>
-        <h6 class="card-first_issue_date">発行日</h6>
-        <h6 class="card-isbn_code">ISBN</h6>
-        <h6 class="card-day_i_read">読んだ日</h6>
-        <h6 class="card-highlight">ハイライト○件</h6>
-      </div>
-    </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="http://localhost/bricolage/public/image/book_cover/WS000002.JPG" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 @endsection
