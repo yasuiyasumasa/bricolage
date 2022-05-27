2d6495ea4584189eb4f330de44fc5726b2c52486
yasuiyasumasa <hikkosisimasita20160821@gmail.com>
2022-05-27 17:22:47 +0900

    2022/05/27

--- resources/views/user/book/create.blade.php
+++ resources/views/user/book/create.blade.php
@@ -2,41 +2,80 @@
 @section('title', '書籍の登録')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>書籍の登録</h2>
-                <form action="{{ action('User\BookController@create') }}" method="post" enctype="multipart/form-data">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <form action="{{ action('User\BookController@create') }}" method="post" enctype="multipart/form-data">
+          @if (count($errors) > 0)
+              <ul>
+                @foreach($errors->all() as $e)
+                  <li>{{ $e }}</li>
+                @endforeach
+            </ul>
+          @endif
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-3 text-md-right">書籍情報の取得</span>
+          </div>
 
-                    @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">本文</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">画像</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
-                    </div>
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
-                </form>
-            </div>
-        </div>
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">Amazon検索</span>
+            <input type="text" class="form-control col-sm-10" id="name" name="name" th:value="*{name}" placeholder="ISBNコード">
+            <span class="col-sm-2"></span>
+            <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('name')}" th:errors="*{name}"></span>
+          </div>
+          <div class="text-center">
+            <button class="btn btn-sm btn-outline-secondary" type="submit">検索</button>
+          </div>
+          <hr>
+
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-3 text-md-right">オリジナル書籍の登録フォーム</span>
+          </div>
+
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">書籍名</span>
+            <input type="text" class="form-control col-sm-10" id="name" name="name" th:value="*{name}" placeholder="書籍名">
+            <span class="col-sm-2"></span>
+            <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('name')}" th:errors="*{name}"></span>
+          </div>
+          
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">ISBNコード</span>
+            <input type="text" class="form-control col-sm-10" id="isbn" name="isbn" th:value="*{isbn}" placeholder="ISBNコード">
+            <span class="col-sm-2"></span>
+            <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('isbn')}" th:errors="*{isbn}"></span>
+          </div>
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">内容</span>
+            <textarea class="form-control col-sm-10" cols="22" rows="5" id="description" name="description"
+              th:text="*{description}" placeholder="内容"></textarea>
+          </div>
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">出版社</span>
+            <input type="text" class="form-control col-sm-10" id="publisher" name="publisher" th:value="*{publisher}" placeholder="出版社">
+            <span class="col-sm-2"></span>
+            <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('publisher')}" th:errors="*{publisher}"></span>
+          </div>
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">価格</span>
+            <input type="number" class="form-control col-sm-10" id="price" name="price" th:value="*{price}" placeholder="価格">
+            <span class="col-sm-2"></span>
+            <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('price')}" th:errors="*{price}"></span>
+          </div>
+          <div class="form-group form-inline input-group-sm">
+            <span class="col-md-2 text-md-right">出版年月日</span>
+            <input type="date" class="form-control col-sm-10" id="publication_date" name="publication_date" th:value="${bookForm.getPublicationDateYmd()}" placeholder="出版年月日">
+            <span class="col-sm-2"></span>
+            <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('publication_date')}" th:errors="*{publication_date}"></span>
+          </div>
+          <div class="text-center">
+            <button class="btn btn-sm btn-outline-secondary" type="submit">登録</button>
+          </div>
+          <input type="hidden" name="id" th:value="*{id}" >
+          <span class="col-sm-10 text-danger small" th:if="${#fields.hasErrors('id')}" th:errors="*{id}"></span>
+          <input type="hidden" name="version" th:value="*{version}" >
+        </form>
+      </div>
     </div>
+  </div>
 @endsection
--- resources/views/user/book/index.blade.php
+++ resources/views/user/book/index.blade.php
@@ -1,6 +1,43 @@
 @extends('layouts.user')
 @section('title', '本棚')
 @section('content')
+<div class="container" th:fragment="search">
+  <form th:action="@{/book/search}" method="get">
+    <div class="form-group form-inline input-group-sm">
+      <label for="name" class="col-md-2 control-label">書籍名</label>
+      <input type="text" class="form-control col-md-5" id="name" name="name" placeholder="書籍名">
+      <label for="isbn" class="col-md-2 control-label">ISBNコード</label>
+      <input type="text" class="form-control col-md-3" id="isbn" name="isbn" placeholder="ISBNコード">
+    </div>
+    <div class="form-group form-inline input-group-sm">
+      <label for="price_from" class="col-md-2 control-label">価格</label>
+      <input type="number" class="form-control col-md-2" id="price_from" name="price_from" placeholder="下限">
+      <label class="col-md-1 control-label">～</label>
+      <input type="number" class="form-control col-md-2" id="price_to" name="price_to" placeholder="上限">
+      <label for="publisher" class="col-md-2 control-label">出版社</label>
+      <input type="text" class="form-control col-md-3" id="publisher" name="publisher" placeholder="出版社">
+    </div>
+    <div class="form-group form-inline input-group-sm">
+      <label for="price_from" class="col-md-2 control-label">出版年月日</label>
+      <input type="date" class="form-control col-md-3" id="publication_date_from" name="publication_date_from" placeholder="From">
+      <label class="col-md-1 control-label">～</label>
+      <input type="date" class="form-control col-md-3" id="publication_date_to" name="publication_date_to" placeholder="To">
+      <div class="col-md-3"></div>
+    </div>
+    <div class="form-group form-inline input-group-sm">
+      <label for="price_from" class="col-md-2 control-label">読んだ日</label>
+      <input type="date" class="form-control col-md-3" id="publication_date_from" name="publication_date_from" placeholder="From">
+      <label class="col-md-1 control-label">～</label>
+      <input type="date" class="form-control col-md-3" id="publication_date_to" name="publication_date_to" placeholder="To">
+      <div class="col-md-3"></div>
+    </div>
+
+    <div class="text-center">
+      <button class="btn btn-sm btn-outline-secondary" type="submit">検索</button>
+    </div>
+  </form>
+  <hr>
+</div>
 <div class="card-deck">
   <div class="card">
     <div class="row no-gutters">
@@ -9,8 +46,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -23,8 +60,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -37,8 +74,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -51,8 +88,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -68,8 +105,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -82,8 +119,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -96,8 +133,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -110,8 +147,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -127,8 +164,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -141,8 +178,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -155,8 +192,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
@@ -169,8 +206,8 @@
       </div>
       <div class="col-lg-6">
         <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">著者　出版社　発行日　ISBN　読んだ日　ハイライト○件</p>
+          <h4 class="card-title">書籍名</h4>
+          <p class="card-text">著者　出版社　出版年月日　ISBN　読んだ日　ハイライト○件</p>
           <a href="#" class="btn btn-light">Go highlight</a>
         </div>
       </div>
